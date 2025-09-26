3b92a16bed38bec58ad83a931d09094de3fc794c
dvsj-lgw <devil.sangjjik2932@gmail.com>
2025-09-26 11:13:53 +0900

    ch4 만들기

---
+++ Ch04/Assets/2.Scripts/CarController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0f;
+    public float decreaseRatio = 0.98f;
+    public float speedRatio = 1000;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;//속도맞추기 프레임
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("deltaTime : " + Time.deltaTime);//프레임 바꾸든 안 바꾸든 일정하게 됨
+        if (Input.GetMouseButtonDown(0))
+        {
+            //this.speed = 0.2f;
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+            this.speed = swipeLength/speedRatio;
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= decreaseRatio;
+    }
+}
