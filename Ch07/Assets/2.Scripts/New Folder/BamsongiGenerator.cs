82d69f34e83074eaca3f15ddecaf8f668fbfdf50
dvsj-lgw <devil.sangjjik2932@gmail.com>
2025-11-12 10:56:26 +0900

    Ch07 밤송이 문제 해결 및 플레이어 이동

--- Ch07/Assets/2.Scripts/New Folder/BamsongiGenerator.cs
+++ Ch07/Assets/2.Scripts/New Folder/BamsongiGenerator.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class BamsongiGenerator : MonoBehaviour
 {
     public GameObject bamsongiPrefab;
+    public float throwForce = 200f;
+    float startValue;
+    float power;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
+            startValue = Input.mousePosition.y;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            power = Input.mousePosition.y - startValue;
+
             GameObject go = Instantiate(bamsongiPrefab);
             go.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 1);
-            go.GetComponent<BamsongiController>().Shoot(new Vector3(0, 600, 2000));
+            go.GetComponent<BamsongiController>().Shoot((transform.forward + transform.up * 0.5f) * throwForce * power);
         }
     }
 }
